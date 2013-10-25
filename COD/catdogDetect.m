7f5e305f9749cdc6c6d9830f536e12db20bbec04
JasonTam <JasonTam22@gmail.com>
2013-10-25 19:52:00 -0400

    lol it doesnt work

--- COD/catdogDetect.m
+++ COD/catdogDetect.m
@@ -4,6 +4,25 @@ addpath(genpath('../'));
 dir_dog = 'n02084071';
 dir_cat = 'n02121620';
 
-dogDetector = vision.CascadeObjectDetector(strcat(dir_dog,'.xml'));
-catDetector = vision.CascadeObjectDetector(strcat(dir_cat,'.xml'));
+dogDetector = vision.CascadeObjectDetector(...
+    'ClassificationModel',strcat(dir_dog,'.xml'),...
+    'MergeThreshold',50);
 
+
+catDetector = vision.CascadeObjectDetector(strcat(dir_cat,'.xml'));
+	
+eyeDetector = vision.CascadeObjectDetector(...
+    'ClassificationModel','EyePairBig');
+
+
+I = imread('../data/cat.1.jpg');
+
+bboxes = step(catDetector, I);
+
+IFaces = insertObjectAnnotation(I, 'rectangle', bboxes, 'DOGE');   
+figure, imshow(IFaces), title('Detected dog');  
+
+[centers, radii, metric] = imfindcircles(I,[8 100]);
+
+
+viscircles(centers, radii,'EdgeColor','b');
